6d3ac2ca9a160f216940b5acf43dc582d96824b4
Rozangela Peixoto <roxpmelo@gmail.com>
2025-08-03 18:45:34 -0300

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/org/ongpatinhas/OngPatinhasApplication.java
@@ -0,0 +1,13 @@
+package org.ongpatinhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngPatinhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngPatinhasApplication.class, args);
+	}
+
+}
