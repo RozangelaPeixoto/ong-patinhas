49dccee3b1b15db11f1c2efe29506775d710e353
Rozangela Peixoto <roxpmelo@gmail.com>
2025-08-14 14:43:15 -0300

    feat: add Donation entity and SQL script for donations table

---
+++ src/main/java/org/ongpatinhas/model/Donation.java
@@ -0,0 +1,39 @@
+package org.ongpatinhas.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+@Table(name="tb_donations")
+public class Donation {
+
+    @Id
+    private UUID id;
+    private String name;
+    private String email;
+    private BigDecimal value;
+    @Column(name="payment_type")
+    private String paymentType;
+    private String status;
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+    @Column(name="id_mercado_pago")
+    private Long idMercadoPago;
+    @Column(name="paid_at")
+    private LocalDateTime paidAt;
+
+}
