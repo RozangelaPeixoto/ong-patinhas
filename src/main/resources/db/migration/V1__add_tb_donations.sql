1a172bbe6f20b94823d28b63e5ab48cd3880e6d3
Rozangela Peixoto <roxpmelo@gmail.com>
2025-08-18 15:38:04 -0300

    feat: update donation table schema to use VARCHAR

--- src/main/resources/db/migration/V1__add_tb_donations.sql
+++ src/main/resources/db/migration/V1__add_tb_donations.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tb_donations (
-   id CHAR(36) PRIMARY KEY,
+   id VARCHAR(36) PRIMARY KEY,
    name VARCHAR(100) NOT NULL,
    email VARCHAR(100) NOT NULL,
    value DECIMAL(10,2) NOT NULL,
