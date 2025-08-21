2d5f749eadc4ce4bde06ee3e1548a551b35ce452
Rozangela Peixoto <roxpmelo@gmail.com>
2025-08-20 23:06:07 -0300

    feat: create AdoptionInterest entity for tracking adoption inquiries

---
+++ src/main/java/org/ongpatinhas/model/AdoptionInterest.java
@@ -0,0 +1,50 @@
+package org.ongpatinhas.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "adoption_interest")
+public class AdoptionInterest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "full_name",nullable = false, length = 100)
+    private String fullName;
+
+    @Column(name = "is_adult", nullable = false)
+    private Boolean isAdult;
+
+    @Column(nullable = false, length = 20)
+    private String phone;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    @Column(name = "pet_name", nullable = false, length = 100)
+    private String petName;
+
+    @Column(name = "had_pets_before")
+    private Boolean hasPetsBefore;
+
+    @Column(name = "currently_has_pets")
+    private Boolean currentlyHasPets;
+
+    @Column(name = "adoption_reason", columnDefinition = "TEXT")
+    private String adoptionReason;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+}
